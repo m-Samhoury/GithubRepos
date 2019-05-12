36fc8c5a2f1f2bbafc4e337f6535bf84b745b3cc
MrSamhoury <moustafa.samhoury@gmail.com>
2019-05-12 22:10:25 +0300

    1)Updated the GithubRepo POKO file to parse more fields to be used in the responses

--- app/src/main/java/com/moustafasamhoury/githubchallenge/models/GithubRepo.kt
+++ app/src/main/java/com/moustafasamhoury/githubchallenge/models/GithubRepo.kt
@@ -12,24 +12,36 @@ import com.squareup.moshi.JsonClass
 data class GithubErrorResponse(
     @field:Json(name = "message") val message: String? = null,
     @field:Json(name = "errors") val githubErrors: List<GithubErrorObject> = listOf()
-)
+) {
+    fun joinErrors() = githubErrors.joinToString(", ") { it.toString() }
+}
 
 @JsonClass(generateAdapter = true)
 data class GithubErrorObject(
     @field:Json(name = "resource") val resource: String,
     @field:Json(name = "field") val field: String,
     @field:Json(name = "code") val code: String
-)
+) {
+    override fun toString(): String {
+        return "Resource: $resource, field:$field, code:$code"
+    }
+}
 
 @JsonClass(generateAdapter = true)
 data class GithubRepo(
+    @field:Json(name = "id") val id: Long,
     @field:Json(name = "items") val name: String? = null,
     @field:Json(name = "language") val programmingLanguage: String? = null,
     @field:Json(name = "description") val description: String? = null,
-    @field:Json(name = "watchers_count") val starsCount: String? = null
+    @field:Json(name = "watchers_count") val starsCount: String? = null,
+    @field:Json(name = "forks_count") val forks: Int
+
 )
 
 @JsonClass(generateAdapter = true)
 data class GithubRepoList(
-    @field:Json(name = "items") val items: List<GithubRepo> = listOf()
+    @field:Json(name = "incomplete_results") val resultIncomplete: Boolean = true,
+    @field:Json(name = "total_count") val total: Int = 0,
+    @field:Json(name = "items") val items: List<GithubRepo> = emptyList(),
+    val nextPage: Int? = null
 )
